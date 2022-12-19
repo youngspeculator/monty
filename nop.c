6f12d288961b2d777b1af731101a6831df7a654a
youngspeculator <jayypalll@gmail.com>
2022-12-19 01:03:53 -0800

    nothing here

---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+ * _nop - nothing
+ * @head: stack head
+ * @line_number: line number
+ * Return: no return
+ */
+void _nop(stack_t **head, unsigned int line_number)
+{
+	(void) counter;
+	(void) head;
+}
