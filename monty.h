4a53df6dca4926c3cd6549fe15d612ea748dec47
youngspeculator <jayypalll@gmail.com>
2022-12-18 21:16:06 -0800

    Header files

--- monty.h
+++ monty.h
@@ -52,7 +52,7 @@ void _add(stack_t **stack, unsigned int line_number);
 void _nop(stack_t **stack, unsigned int line_number);
 void _sub(stack_t **stack, unsigned int line_number);
 stack_t *add_dnodeint_end(stack_t **head, const int n);
-stack_t *add_dnodeint(stack_t *head);
+stack_t *add_dnodeint(stack_t **head);
 void free_dlistint(stack_t *head);
 int delete_dnodeint_at_index(stack_t **head, unsigned int index);
 void error_exit(stack_t **stack);
