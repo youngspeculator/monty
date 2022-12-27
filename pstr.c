46caa6e7e61a13efa7bd1b33e74c26d64e562c4a
youngspeculator <jayypalll@gmail.com>
2022-12-27 05:09:55 -0800

    pstr

---
+++ pstr.c
@@ -0,0 +1,25 @@
+#include "monty.h"
+
+/**
+ * f_pstr - prints the string started at the top of the stack
+ * @head: stack head
+ * @counter: line number
+ * Return: no return
+*/
+void f_pstr(stack_t **head, unsigned int counter)
+{
+	stack_t *h;
+	(void) counter;
+
+	h = head;
+	while (h)
+	{
+		if (h->n > 127 || h->n <= 0)
+		{
+			break;
+		}
+		printf("%c", h->n);
+		h = h->next;
+	}
+	printf("\n");
+}
