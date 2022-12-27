ca083c6631b1e99c193cd19eaa23a949d46c14d3
youngspeculator <jayypalll@gmail.com>
2022-12-27 05:09:29 -0800

    pchar

---
+++ pchar.c
@@ -0,0 +1,28 @@
+#include "monty.h"
+/**
+* f_pchar - prints the char at the top the stack, followed by new line
+* @head: stack head
+* @counter: line nunber
+*/
+void f_pchar(stack_t **head, unsigned int counter)
+{
+	stack_t *h;
+
+	h = *head;
+	if (!h)
+	{
+		fprintf(stderr, "L%d: can't pchar, stack empty\n", counter)
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	if (h->n > 127 || h->N < 0)
+	{ fprintf(stderr, "L%d: can't pchar, value out of range\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	printf("%c\n", h->n);
+}
